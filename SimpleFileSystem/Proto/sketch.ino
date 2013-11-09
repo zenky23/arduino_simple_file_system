634a1431dfd06c0a74d160498fa0b47c322c3cd5
Mike C <mcrosson_git at nusku dot net>
2013-11-09 13:15:59 -0500

    Added basic sketch that interfaces with SPI SRAM

---
+++ SimpleFileSystem/Proto/sketch.ino
@@ -0,0 +1,48 @@
+// http://ucexperiment.wordpress.com/2013/02/23/interfacing-microchip-23k256-32kb-sram-spi-memory-to-the-arduino/
+
+#include <SPI.h>
+ 
+//SRAM opcodes
+#define RDSR 5
+#define WRSR 1
+#define READ 3
+#define WRITE 2
+ 
+uint8_t SpiRAMRead8(uint16_t address) {
+  uint8_t read_byte;
+  SPI.transfer(4, READ, SPI_CONTINUE);
+  SPI.transfer(4, (char)(address >> 8), SPI_CONTINUE);
+  SPI.transfer(4, (char)address, SPI_CONTINUE);
+  read_byte = SPI.transfer(4, 0xFF);
+  return read_byte;
+}
+ 
+void SpiRAMWrite8(uint16_t address, uint8_t data_byte) {
+  SPI.transfer(4, WRITE, SPI_CONTINUE);
+  SPI.transfer(4, (char)(address >> 8), SPI_CONTINUE);
+  SPI.transfer(4, (char)address, SPI_CONTINUE);
+  SPI.transfer(4, data_byte);
+}
+ 
+void setup(void) {
+  uint16_t addr;
+  uint8_t i;
+ 
+  Serial.begin(9600);
+  pinMode(4, INPUT_PULLUP); // Test from website
+  SPI.begin(4);
+  int dataByte = 255;
+  for (addr=0; addr<32; addr++) {
+    Serial.print("Write: ");
+    SpiRAMWrite8(addr, (uint8_t)dataByte);
+    dataByte--;
+    Serial.print("Addr: ");
+    Serial.print(addr);
+    i = SpiRAMRead8(addr);
+    Serial.print(" | Read: ");
+    Serial.println((uint16_t)i);
+  }
+}
+ 
+void loop() {
+}
